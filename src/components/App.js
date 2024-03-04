3f492df6d6d49ced3842cbe7c338c56bd624c7c5
widemaks <derlaster@yandex.ru>
2024-03-04 21:37:46 +0300

    init layout components

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './layout/header/Header';
+import Main from './layout/main/Main';
+import Footer from './layout/footer/Footer';
+
+function App() {
+  return (
+    <div className='root-app'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/layout/footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+      <p>Подвал</p>
+    </footer>
+  )
+}
+
+export default Footer
